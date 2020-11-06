f466bbc8da36ac8cf67b386b01f2483a8c9d244d
ViktorHagstrom <viktor.hagstrom@hotmail.se>
2020-11-06 10:18:48 +0100

    hejhej

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,37 @@
+import java.util.Calendar;
+
 public class Person {
+
+    // Instansvariabler
     String firstName;
     String lastName;
     String dateOfBirth;
-    double height;
+    public double height;
     double weight;
 
-    public static String getName(Person p){
 
-        return p.firstName + " " + p.lastName;
+    // Instansmetoder - får ej innehålla static
+    public String getName() {
+        return this.firstName + " " + this.lastName;
     }
+
+    public int getAge() {
+
+        String calendar = Calendar.getInstance().getTime().toString();
+        String thisYearString = calendar.substring(calendar.lastIndexOf(' ') + 1);
+        int thisYear = Integer.parseInt(thisYearString);
+
+        String yearString = this.dateOfBirth.substring(0, 4);
+        int year = Integer.parseInt(yearString);
+
+        int age = thisYear - year;
+        return age;
+    }
+
+    public double getBMI() {
+        return this.weight / (this.height * 2);
+    }
+
+
 }
+
